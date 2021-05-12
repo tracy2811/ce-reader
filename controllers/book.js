83879a81a64a42a0086e69eb16da8cf3a0a98228
Trang Nguyen <trangnguyen.281198@gmail.com>
2021-05-12 15:21:16 +0300

    Add create-book, delete-book, create-chapter, delete-chapter functions

--- controllers/book.js
+++ controllers/book.js
@@ -1,4 +1,6 @@
 var fs = require("fs").promises;
+var constants = require("fs").constants;
+var createError = require("http-errors");
 var { tokenizeFile, tokenizeText } = require("../utils/tokenize");
 var { getHistory, updateHistory, saveHistory } = require("../utils/history");
 
@@ -17,6 +19,184 @@ const get_book_list = async (req, res, next) => {
   }
 };
 
+const get_delete_book = async (req, res, next) => {
+  try {
+    const { book } = req.params;
+    const dir = `${__dirname}/../public/books/${book}`;
+    await fs.access(dir, constants.R_OK & constants.W_OK);
+    const data = await fs.readdir(dir);
+    res.render("book-delete", {
+      title: `${book} | Delete`,
+      book: {
+        url: `/${book}`,
+        title: book,
+      },
+      chapter_list: data.map((d) => ({
+        url: `/${book}/${d}`,
+        title: d,
+      })),
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
+};
+
+const post_delete_book = async (req, res, next) => {
+  try {
+    const { book } = req.params;
+    const filename = `${__dirname}/../public/books/${book}`;
+    await fs.rm(filename, { recursive: true, force: true });
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
+};
+
+const get_create_book = async (req, res, next) => {
+  res.render("book-form", { title: "Create Book" });
+};
+
+const post_create_book = async (req, res, next) => {
+  const { title } = req.body;
+  const fail = () =>
+    res.render("book-form", {
+      title: "Create Book",
+      error: "Invalid book title",
+    });
+  if (!title) {
+    fail();
+    return;
+  }
+  const sanitized_title = title.trim().replace(/\s+/g, " ").replace(/\//g, "");
+  if (
+    !sanitized_title ||
+    sanitized_title == "create-book" ||
+    sanitized_title.toLowerCase() == "delete"
+  ) {
+    fail();
+    return;
+  }
+  const dir = `${__dirname}/../public/books/${sanitized_title}`;
+  const success = () => res.redirect(`/${sanitized_title}`);
+  try {
+    await fs.access(dir, constants.R_OK & constants.W_OK);
+    success();
+    return;
+  } catch (error) {
+    try {
+      await fs.mkdir(dir);
+      success();
+    } catch (error) {
+      next(error);
+    }
+  }
+};
+
+const get_create_book_chapter = async (req, res, next) => {
+  const { book } = req.params;
+  const dir = `${__dirname}/../public/books/${book}`;
+  try {
+    await fs.access(dir, constants.R_OK & constants.W_OK);
+    res.render("chapter-form", {
+      title: `${book} | Create Chapter`,
+      book: {
+        url: `/${book}`,
+        title: book,
+      },
+    });
+  } catch (error) {
+    next(createError(404));
+  }
+};
+
+const post_create_book_chapter = async (req, res, next) => {
+  const { book } = req.params;
+  const { title, content } = req.body;
+  const fail = () =>
+    res.render("chapter-form", {
+      title: `${book} | Create Chapter`,
+      book: {
+        url: `/${book}`,
+        title: book,
+      },
+      error: "Invalid chapter title or content",
+    });
+  const dir = `${__dirname}/../public/books/${book}`;
+  try {
+    await fs.access(dir, constants.R_OK & constants.W_OK);
+    if (!title || !content) {
+      fail();
+      return;
+    }
+    const sanitized_title = title
+      .trim()
+      .replace(/\s+/g, " ")
+      .replace(/\//g, "");
+    const sanitized_content = content
+      .trim()
+      .split("\n")
+      .map((p) => p.trim().replace(/\s+/g, " "))
+      .filter((p) => !!p)
+      .join("\n");
+    if (
+      !sanitized_title ||
+      sanitized_title == "create-chapter" ||
+      sanitized_title == "delete" ||
+      !sanitized_content
+    ) {
+      fail();
+      return;
+    }
+    const filename = `${__dirname}/../public/books/${book}/${sanitized_title}`;
+    try {
+      await fs.access(filename, constants.R_OK);
+      fail();
+      return;
+    } catch (error) {
+      try {
+        await fs.writeFile(filename, sanitized_content);
+        res.redirect(`/${book}/${sanitized_title}`);
+      } catch (error) {
+        next(error);
+      }
+    }
+  } catch (error) {
+    next(createError(404));
+  }
+};
+
+const get_delete_book_chapter = async (req, res, next) => {
+  try {
+    const { book, chapter } = req.params;
+    const filename = `${__dirname}/../public/books/${book}/${chapter}`;
+    await fs.access(filename, constants.R_OK & constants.W_OK);
+    res.render("chapter-delete", {
+      title: `${book} | ${chapter} | Delete`,
+      book: {
+        url: `/${book}`,
+        title: book,
+      },
+      chapter: {
+        url: `/${book}/${chapter}`,
+        title: chapter,
+      },
+    });
+  } catch (error) {
+    res.redirect(`/${book}`);
+  }
+};
+
+const post_delete_book_chapter = async (req, res, next) => {
+  try {
+    const { book, chapter } = req.params;
+    const filename = `${__dirname}/../public/books/${book}/${chapter}`;
+    await fs.rm(filename, { recursive: true, force: true });
+    res.redirect(`/${book}`);
+  } catch (error) {
+    next(error);
+  }
+};
+
 const get_book_chapter_detail = async (req, res, next) => {
   try {
     const { book, chapter } = req.params;
@@ -26,7 +206,7 @@ const get_book_chapter_detail = async (req, res, next) => {
     const title = `${book} | ${chapter}`;
     const tokenized_title = tokenizeText(title);
     const history = getHistory();
-    res.render("book-chapter-detail", {
+    res.render("chapter-detail", {
       title,
       tokenized_title,
       book: {
@@ -37,13 +217,18 @@ const get_book_chapter_detail = async (req, res, next) => {
         title: chapter,
         url: `/${book}/${chapter}`,
         paragraphs,
-        completed: history[book] && history[book][chapter] ? history[book][chapter].completed : false,
+        completed:
+          history[book] && history[book][chapter]
+            ? history[book][chapter].completed
+            : false,
         bookmark:
-          history[book] && history[book][chapter] ? history[book][chapter].bookmark : -1,
+          history[book] && history[book][chapter]
+            ? history[book][chapter].bookmark
+            : -1,
       },
     });
   } catch (error) {
-    next(error);
+    next(createError(404));
   }
 };
 
@@ -53,7 +238,7 @@ const get_book_chapter_list = async (req, res, next) => {
     const data = await fs.readdir(`${__dirname}/../public/books/${book}`);
     const history = getHistory();
     const tokenized_title = tokenizeText(book);
-    res.render("book-chapter-list", {
+    res.render("chapter-list", {
       tokenized_title,
       title: book,
       book: {
@@ -63,11 +248,14 @@ const get_book_chapter_list = async (req, res, next) => {
       chapter_list: data.map((d) => ({
         url: `/${book}/${d}`,
         title: d,
-        completed: history[book] && history[book][d] ? history[book][d].completed : false,
+        completed:
+          history[book] && history[book][d]
+            ? history[book][d].completed
+            : false,
       })),
     });
   } catch (error) {
-    next(error);
+    next(createError(404));
   }
 };
 
@@ -99,4 +287,12 @@ module.exports = {
   get_book_chapter_detail,
   get_book_chapter_list,
   put_history,
+  get_create_book,
+  get_create_book_chapter,
+  post_create_book,
+  post_create_book_chapter,
+  get_delete_book,
+  post_delete_book,
+  get_delete_book_chapter,
+  post_delete_book_chapter,
 };
--- routes/books.js
+++ routes/books.js
@@ -8,7 +8,15 @@ var bookController = require("../controllers/book");
 // });
 
 router.get("/", bookController.get_book_list);
+router.get("/create-book", bookController.get_create_book);
+router.post("/create-book", bookController.post_create_book);
 router.get("/:book", bookController.get_book_chapter_list);
+router.get("/:book/delete", bookController.get_delete_book);
+router.post("/:book/delete", bookController.post_delete_book);
+router.get("/:book/create-chapter", bookController.get_create_book_chapter);
+router.post("/:book/create-chapter", bookController.post_create_book_chapter);
+router.get("/:book/:chapter/delete", bookController.get_delete_book_chapter);
+router.post("/:book/:chapter/delete", bookController.post_delete_book_chapter);
 router.get("/:book/:chapter", bookController.get_book_chapter_detail);
 router.put("/history", bookController.put_history);
 
